d1b5d777a32a7800345319659c884e1c636a5b76
Crowdbotics <team@crowdbotics.com>
2021-04-06 21:34:31 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/aero_2_25498/MainActivity.java
@@ -0,0 +1,15 @@
+package com.aero_2_25498;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "aero_2_25498";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://aero-2-25498.botics.co",
+  defaultTimeout: 5000
+};
